a2a81460656e084fae7d2ae48a58625b5d470273
asadawan16 <asadawan16900@gmail.com>
2025-02-03 00:51:06 +0500

    updated cors configuration

--- Frontend/src/axiosInstance.js
+++ Frontend/src/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://blingg-jewelery-backend.vercel.app",
+  baseURL:
+    "blingg-jewelery-backend-jjgxc4j4z-asadullahtalats-projects.vercel.app",
   withCredentials: true, // Include cookies if using authentication
 });
 
