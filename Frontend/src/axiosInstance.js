9fa9de9bfbf5d0027126f2cdc439b0a4d482ca51
asadawan16 <asadawan16900@gmail.com>
2025-02-03 10:53:38 +0500

    updated cors configuration

--- Frontend/src/axiosInstance.js
+++ Frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true, // Include cookies if using authentication
 });
 
