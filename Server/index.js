abc9cbac8ae939089e75ac7f67e40012867036d8
asadawan16 <asadawan16900@gmail.com>
2025-02-03 13:22:20 +0500

    modified Server code

--- Server/index.js
+++ Server/index.js
@@ -12,16 +12,15 @@ const UserModel = require("./models/user");
 const OrderModel = require("./models/Orders");
 const CartModel = require("./models/Cart");
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    accessControlAllowOrigin: "*",
-    origin: "https://blingg-jewelery.vercel.app", // Allow requests only from your frontend
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: "https://blingg-jewelery.vercel.app", // Replace with your frontend's URL
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
@@ -34,10 +33,6 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello Server is Running");
-});
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
 });
 
 const storage = multer.diskStorage({
@@ -300,6 +295,4 @@ app.delete("/clearcart", authenticate, async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-module.exports = app;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
