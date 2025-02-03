4b610291da94d8c42cff24b105dfaea19f90290f
asadawan16 <asadawan16900@gmail.com>
2025-02-03 11:24:53 +0500

    updated backend code v2

--- Frontend/src/axiosInstance.js
+++ Frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://blingg-jewelery-backend.vercel.app",
   withCredentials: true, // Include cookies if using authentication
 });
 
