6ea1f78895c7e7bc14688b10f1fd6cc7e983c44d
asadawan16 <asadawan16900@gmail.com>
2025-02-04 15:43:43 +0500

    User Specific Order

--- Frontend/src/Store/Order-actions.js
+++ Frontend/src/Store/Order-actions.js
@@ -1,7 +1,11 @@
 import { orderActions } from "./Order-Slice";
 import API from "../axiosInstance";
+const user = sessionStorage.getItem("user");
 export const fetchOrderData = () => {
   return async (dispatch) => {
+    if (!user.role === "admin") {
+      return;
+    }
     try {
       const response = await API.get("/all-orders", {
         headers: {
@@ -24,6 +28,7 @@ export const fetchUserOrder = () => {
         },
       });
       dispatch(orderActions.setOrder(response.data));
+      console.log("specific order", response.data);
     } catch (error) {
       console.log("error fetching user Orders", error.message);
     }
