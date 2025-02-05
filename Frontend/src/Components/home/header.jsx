db68802405b3f88213e76ffc6dcc26f9176b43cb
asadawan16 <asadawan16900@gmail.com>
2025-02-05 20:49:17 +0500

    updated fav icon

--- Frontend/src/Components/home/header.jsx
+++ Frontend/src/Components/home/header.jsx
@@ -65,7 +65,7 @@ const Header = () => {
             {isAuthenticated && (
               <li>
                 <NavLink
-                  to="/myorders"
+                  to="/orders"
                   className={({ isActive }) => (isActive ? "active" : "")}
                   onClick={() => setMobileNavOpen(false)}
                 >
