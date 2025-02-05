eb1e9b6237e9f5584ae85d64ab93c7bcce9db1bb
asadawan16 <asadawan16900@gmail.com>
2025-02-05 14:56:36 +0500

    Responsive Css Added for home and products

--- Frontend/src/Components/cart/cart.jsx
+++ Frontend/src/Components/cart/cart.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./cart.css";
 import { IoMdArrowBack } from "react-icons/io";
@@ -8,15 +7,18 @@ import { cartActions } from "../../Store/cart-slice";
 const Cart = () => {
   const dispatch = useDispatch();
   const cartdata = useSelector((state) => state.cart);
+
   return (
     <>
       <Link to={"/products"} className="backtoproducts">
         <IoMdArrowBack className="back-arrow" />
         Back
       </Link>
+
       <div className="cart">
         <div className="container">
           <h2>CART</h2>
+
           {cartdata.items.length === 0 ? (
             <>
               <h3>Cart is empty</h3>
@@ -24,58 +26,65 @@ const Cart = () => {
             </>
           ) : (
             <>
-              {" "}
-              <ul className="cart-grid">
-                <li className="cart-item" id="cart-header">
-                  <h4>Product</h4>
-                  <span>Price</span>
-                  <span>Quantity</span>
-                  <span></span>
-                  <span>Total</span>
-                </li>
-                {cartdata.items.map((item) => (
-                  <li className="cart-item" key={item.id}>
-                    <h4>{item.name}</h4>
-                    <span>${item.price}</span>
-                    <div className="quantity">
-                      <button
-                        type="button"
-                        onClick={() =>
-                          dispatch(cartActions.removeItemFromCart(item.id))
-                        }
-                      >
-                        -
-                      </button>
-                      <input
-                        type="number"
-                        name=""
-                        id=""
-                        min="1"
-                        value={item.quantity}
-                        readOnly
-                      />
-                      <button
-                        type="button"
-                        onClick={() =>
-                          dispatch(
-                            cartActions.addItemToCart({
-                              id: item.id,
-                              title: item.title,
-                              price: item.price,
-                            })
-                          )
-                        }
-                      >
-                        +
-                      </button>
-                    </div>
-                    <span>${item.totalPrice}</span>
-                  </li>
-                ))}
-              </ul>
+              <table className="cart-grid">
+                <thead id="cart-header">
+                  <th>Product</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th className="itemtotalprice">Total</th>
+                </thead>
+                <tbody>
+                  {cartdata.items.map((item) => (
+                    <tr className="cart-item" key={item.id}>
+                      <td>
+                        <span>{item.name}</span>
+                      </td>
+                      <td>
+                        <span>${item.price}</span>
+                      </td>
+                      <td>
+                        <div className="quantity">
+                          <button
+                            type="button"
+                            onClick={() =>
+                              dispatch(cartActions.removeItemFromCart(item.id))
+                            }
+                          >
+                            -
+                          </button>
+                          <input
+                            type="number"
+                            min="1"
+                            value={item.quantity}
+                            readOnly
+                          />
+                          <button
+                            type="button"
+                            onClick={() =>
+                              dispatch(
+                                cartActions.addItemToCart({
+                                  id: item.id,
+                                  name: item.name,
+                                  price: item.price,
+                                })
+                              )
+                            }
+                          >
+                            +
+                          </button>
+                        </div>
+                      </td>
+                      <td className="itemtotalprice">
+                        <span>${item.totalPrice}</span>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
               <h3>Total: ${cartdata.totalPayment}</h3>
             </>
           )}
+
           {cartdata.items.length !== 0 && (
             <Link to="/placeorder" className="checkoutbtn">
               Checkout
